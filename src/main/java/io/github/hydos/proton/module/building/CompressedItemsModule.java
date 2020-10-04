6989a614eb580c1932be008a0df974a651ded693
Yonatan Dafna <yonatandafna1@gmail.com>
2020-10-04 22:45:06 +0300

    (Maybe) Perfectly balanced. As all things should be (no. 2)

--- src/main/java/io/github/hydos/proton/module/building/CompressedItemsModule.java
+++ src/main/java/io/github/hydos/proton/module/building/CompressedItemsModule.java
@@ -52,7 +52,7 @@ public class CompressedItemsModule extends Module {
 
             DARK_BLUE_ICE = ProtonRegisterUtil.block("dark_blue_ice",
                                                  new Block(FabricBlockSettings.copyOf(Blocks.BLUE_ICE)
-                                                                              .slipperiness(1.23F)),
+                                                                              .slipperiness(1.234F)),
                                                  new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)
                                                 );
         }
