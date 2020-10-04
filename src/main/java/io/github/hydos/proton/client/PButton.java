1a9977105553bb4417d764570674e43f7ceb4ce8
Dźwiedź <kg67199@gmail.com>
2020-10-04 12:09:15 +0200

    let the user hit p; added gay 🏳️‍🌈

---
+++ src/main/java/io/github/hydos/proton/client/PButton.java
@@ -0,0 +1,43 @@
+package io.github.hydos.proton.client;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.options.OptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.math.MathHelper;
+
+import java.awt.*;
+import java.util.Calendar;
+
+public class PButton extends ButtonWidget {
+    private final boolean gay;
+    private float tick = 0;
+
+    public PButton(int x, int y) {
+        super(x, y, 20, 20, new LiteralText("p"), PButton::click);
+        gay = Calendar.getInstance().get(Calendar.MONTH) == Calendar.JUNE;
+    }
+
+    @Override
+    public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        client.getTextureManager().bindTexture(WIDGETS_LOCATION);
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, this.alpha);
+        int i = this.getYImage(this.isHovered());
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.enableDepthTest();
+        this.drawTexture(matrices, this.x, this.y, 0, 46 + i * 20, this.width / 2, this.height);
+        this.drawTexture(matrices, this.x + this.width / 2, this.y, 200 - this.width / 2, 46 + i * 20, this.width / 2, this.height);
+        this.renderBg(matrices, client, mouseX, mouseY);
+        int color = gay ? Color.HSBtoRGB((tick+=delta / 200F), 1F, 1F) : 0x48DDBC;
+        drawCenteredText(matrices, client.textRenderer, this.getMessage(), this.x + this.width / 2, this.y + (this.height - 8) / 2, color | MathHelper.ceil(this.alpha * 255.0F) << 24);
+    }
+
+    public static void click(ButtonWidget _buttonWidget) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        client.openScreen(new OptionsScreen(client.currentScreen, client.options));
+    }
+}
--- src/main/java/io/github/hydos/proton/mixin/client/TitleScreenMixin.java
+++ src/main/java/io/github/hydos/proton/mixin/client/TitleScreenMixin.java
@@ -1,14 +1,15 @@
 package io.github.hydos.proton.mixin.client;
 
+import com.google.common.collect.ImmutableSet;
+import io.github.hydos.proton.client.PButton;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.gui.screen.options.OptionsScreen;
+import net.minecraft.client.gui.widget.AbstractButtonWidget;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.text.LiteralText;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,11 +24,16 @@ public class TitleScreenMixin extends Screen {
     }
 
     @Inject(method = "init", at = @At("TAIL"))
-    public void addQButton(CallbackInfo ci){
-        this.addButton(new ButtonWidget(89, 156, 20, 20, new LiteralText("q").formatted(Formatting.DARK_AQUA), (buttonWidget) -> {
-            this.client.openScreen(new OptionsScreen(this, this.client.options));
-        }));
-    }
+    public void addPButton(CallbackInfo ci) {
+        ImmutableSet<String> targets = ImmutableSet.of(I18n.translate("menu.online"));
 
+        for (AbstractButtonWidget button : buttons) {
+            if (targets.contains(button.getMessage().getString())) {
+                ButtonWidget p = new PButton(button.x - 24, button.y);
+                this.addButton(p);
+                return;
+            }
+        }
+    }
 
 }
