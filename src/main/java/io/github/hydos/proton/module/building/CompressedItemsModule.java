8c2a6e461b24bb65f8eff28d05ca65cca11ba806
Yonatan Dafna <yonatandafna1@gmail.com>
2020-10-04 20:35:24 +0300

    Added the compressed nether star

---
+++ src/main/java/io/github/hydos/proton/module/building/CompressedItemsModule.java
@@ -0,0 +1,45 @@
+package io.github.hydos.proton.module.building;
+
+import io.github.hydos.proton.Proton;
+import io.github.hydos.proton.config.Configurable;
+import io.github.hydos.proton.module.Module;
+import io.github.hydos.proton.util.ProtonRegisterUtil;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Rarity;
+
+public class CompressedItemsModule extends Module {
+    @Configurable
+    public static boolean compressed_nether_star = true;
+
+    public CompressedItemsModule() {
+        super(Proton.identifier("compressed_items"));
+    }
+
+    @Override
+    public void commonInit() {
+        ModuleBlocks.register();
+    }
+
+    public static class ModuleBlocks {
+        public static Block COMPRESSED_NETHER_STAR;
+
+        public static void register() {
+            if (compressed_nether_star) {
+                COMPRESSED_NETHER_STAR = ProtonRegisterUtil.block(
+                        "compressed_nether_star",
+                        new Block(FabricBlockSettings.of(Material.STONE)
+                                                     .breakByHand(false)
+                                                     .requiresTool()
+                                                     .breakByTool(FabricToolTags.PICKAXES, 2)
+                                                     .strength(3.0F, 1200.0F)),
+                        new FabricItemSettings().group(ItemGroup.DECORATIONS).maxCount(64).rarity(Rarity.RARE)
+                                                                 );
+            }
+        }
+    }
+}
