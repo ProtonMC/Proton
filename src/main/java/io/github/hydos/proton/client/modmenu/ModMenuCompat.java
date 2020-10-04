62cdb02afa551587d8ecbe4132b8af1d13b79879
Yonatan Dafna <yonatandafna1@gmail.com>
2020-10-04 13:37:42 +0300

    Stuff

---
+++ src/main/java/io/github/hydos/proton/client/modmenu/ModMenuCompat.java
@@ -0,0 +1,20 @@
+package io.github.hydos.proton.client.modmenu;
+
+import io.github.hydos.proton.Proton;
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ModMenuCompat implements ModMenuApi {
+    @Override
+    public String getModId() {
+        return Proton.MOD_ID;
+    }
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> null;
+    }
+}
