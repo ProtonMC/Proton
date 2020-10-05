061f00b3232bfe521f93595784ea3d2fa8724fd1
dyon <dyon@dyonb.nl>
2020-10-05 18:49:18 +0200

    Add Turf block

--- src/main/java/io/github/hydos/proton/base/block/ProtonSlabBlock.java
+++ src/main/java/io/github/hydos/proton/base/block/ProtonSlabBlock.java
@@ -3,18 +3,12 @@ package io.github.hydos.proton.base.block;
 import io.github.hydos.proton.util.ProtonRegisterUtil;
 import io.github.hydos.proton.util.VariantHandler;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
-import net.minecraft.world.BlockView;
 
 public class ProtonSlabBlock extends SlabBlock {
 
---
+++ src/main/java/io/github/hydos/proton/module/building/TurfModule.java
@@ -0,0 +1,41 @@
+package io.github.hydos.proton.module.building;
+
+import io.github.hydos.proton.Proton;
+import io.github.hydos.proton.module.ProtonModule;
+import io.github.hydos.proton.util.ProtonRegisterUtil;
+import io.github.hydos.proton.util.VariantHandler;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.client.color.world.GrassColors;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class TurfModule extends ProtonModule {
+    public TurfModule() {
+        super(Proton.identifier("turf"));
+    }
+
+    public static Block turfBlock;
+
+    @Override
+    public void commonInit() {
+        turfBlock = new Block(AbstractBlock.Settings.copy(Blocks.GRASS_BLOCK));
+        ProtonRegisterUtil.block("turf", turfBlock, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+        VariantHandler.addSlabAndStairs(turfBlock);
+    }
+
+    @Override
+    public void clientInit() {
+        Block turf_slab = Registry.BLOCK.get(new Identifier(Registry.BLOCK.getId(turfBlock).toString() + "_slab"));
+        Block turf_stairs = Registry.BLOCK.get(new Identifier(Registry.BLOCK.getId(turfBlock).toString() + "_stairs"));
+
+        ColorProviderRegistry.BLOCK.register((blockState, renderView, blockPos, tintIndex) -> {
+            return renderView != null ? BiomeColors.getGrassColor(renderView, blockPos) : GrassColors.getColor(0.5D, 1.0D);
+        }, turfBlock, turf_slab, turf_stairs);
+    }
+}
