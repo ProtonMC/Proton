9aebc5bf45956a2b39f8f3a20b08c2fc79789ad8
hydos <haydenv06@gmail.com>
2020-10-05 10:10:17 +1000

    fix shiny module crash

--- src/main/java/io/github/hydos/proton/mixin/client/entities/LlamaRendererMixin.java
+++ src/main/java/io/github/hydos/proton/mixin/client/entities/LlamaRendererMixin.java
@@ -7,7 +7,9 @@ import net.minecraft.client.render.entity.RabbitEntityRenderer;
 import net.minecraft.entity.passive.LlamaEntity;
 import net.minecraft.entity.passive.RabbitEntity;
 import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,10 +17,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LlamaEntityRenderer.class)
 public class LlamaRendererMixin {
 
+    @Shadow @Final private static Identifier[] TEXTURES;
+
     @Inject(method = "getTexture", at = @At("HEAD"), cancellable = true)
     public void getTypeTexture(LlamaEntity llamaEntity, CallbackInfoReturnable<Identifier> cir){
         if(ModuleManager.getInstance().isModuleEnabled(VariantAnimalTexturesModule.class)){
-            cir.setReturnValue(VariantAnimalTexturesModule.getTextureOrShiny(llamaEntity, VariantAnimalTexturesModule.VariantTextureType.LLAMA, VariantAnimalTexturesModule.enableShinyLlama));
+            cir.setReturnValue(VariantAnimalTexturesModule.getTextureOrShiny(llamaEntity, VariantAnimalTexturesModule.VariantTextureType.RABBIT, () -> getOldTexture(llamaEntity)));
         }
+    }
+
+    private Identifier getOldTexture(LlamaEntity llamaEntity) {
+        return TEXTURES[llamaEntity.getVariant()];
     }
 }
