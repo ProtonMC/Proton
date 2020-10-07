584703c003f4ea1ac9dcceeba3776d40891d11a1
Yonatan Dafna <yonatandafna1@gmail.com>
2020-10-07 11:43:11 +0300

    Added target specification

--- src/main/java/io/github/protonmc/proton/base/annotation/FromModule.java
+++ src/main/java/io/github/protonmc/proton/base/annotation/FromModule.java
@@ -2,6 +2,10 @@ package io.github.protonmc.proton.base.annotation;
 
 import io.github.protonmc.proton.module.ProtonModule;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD})
 public @interface FromModule {
     Class<? extends ProtonModule>[] value();
 }
