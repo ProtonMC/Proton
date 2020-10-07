56cc80a3c9afd4eb3a2b563ae7daa06a647109ea
dyon <dyon@dyonb.nl>
2020-10-07 09:33:03 +0200

    Make VariantHandler allow multiple blocks.

--- src/main/java/io/github/protonmc/proton/base/handler/VariantHandler.java
+++ src/main/java/io/github/protonmc/proton/base/handler/VariantHandler.java
@@ -6,6 +6,7 @@ import io.github.protonmc.proton.base.block.ProtonWallBlock;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -15,26 +16,32 @@ public class VariantHandler {
     public static final List<ProtonStairsBlock> STAIRS = new LinkedList<>();
     public static final List<ProtonWallBlock> WALLS = new LinkedList<>();
 
-    public static void addSlabStairsWall(Block block) {
-        addSlabAndStairs(block);
-        addWall(block);
+    public static void addSlabStairsWall(Block... blocks) {
+        addSlabAndStairs(blocks);
+        addWall(blocks);
     }
 
-    public static void addSlabAndStairs(Block block) {
-        addSlab(block);
-        addStairs(block);
+    public static void addSlabAndStairs(Block... blocks) {
+        addSlab(blocks);
+        addStairs(blocks);
     }
 
-    public static void addSlab(Block block) {
-        SLABS.add(new ProtonSlabBlock(block));
+    public static void addSlab(Block... blocks) {
+        Arrays.stream(blocks).forEach(block -> {
+            SLABS.add(new ProtonSlabBlock(block));
+        });
     }
 
-    public static void addStairs(Block block) {
-        STAIRS.add(new ProtonStairsBlock(block));
+    public static void addStairs(Block... blocks) {
+        Arrays.stream(blocks).forEach(block -> {
+            STAIRS.add(new ProtonStairsBlock(block));
+        });
     }
 
-    public static void addWall(Block block) {
-        WALLS.add(new ProtonWallBlock(block));
+    public static void addWall(Block... blocks) {
+        Arrays.stream(blocks).forEach(block -> {
+            WALLS.add(new ProtonWallBlock(block));
+        });
     }
 
     public static AbstractBlock.Settings realStateCopy(Block parent) {
