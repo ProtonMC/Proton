867218c9fb7815f81893a786d4100ae3b428258d
Yonatan Dafna <yonatandafna1@gmail.com>
2020-10-08 14:12:04 +0300

    Added Javadoc to CompressedNetherStarItem

--- src/main/java/io/github/protonmc/proton/module/building/common/item/CompressedNetherStarItem.java
+++ src/main/java/io/github/protonmc/proton/module/building/common/item/CompressedNetherStarItem.java
@@ -7,11 +7,18 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Rarity;
 
+/**
+ * Class for the compressed nether star item.
+ * @author YTG1234
+ */
 public class CompressedNetherStarItem extends BlockItem {
     public CompressedNetherStarItem() {
         super(CompressedItemsModule.ModuleBlocks.COMPRESSED_NETHER_STAR, new FabricItemSettings().group(ItemGroup.DECORATIONS).rarity(Rarity.RARE));
     }
 
+    /**
+     * @see BlockItem#damage(DamageSource)
+     */
     @Override
     public boolean damage(DamageSource source) {
         if (source.isExplosive()) return false;
