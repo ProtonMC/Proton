47964e4d98777a28a4b9b7fc7c4ef2a103d888fe
kara-b <dyon@dyonb.nl>
2020-10-14 10:46:02 +0200

    Simple block loot generator

--- src/main/java/io/github/protonmc/proton/base/handler/DataHandler.java
+++ src/main/java/io/github/protonmc/proton/base/handler/DataHandler.java
@@ -1,9 +1,11 @@
 package io.github.protonmc.proton.base.handler;
 
+import com.google.common.collect.ImmutableSet;
 import com.swordglowsblue.artifice.api.Artifice;
 import com.swordglowsblue.artifice.api.ArtificeResourcePack;
 import io.github.protonmc.proton.base.module.ModuleManager;
 import io.github.protonmc.proton.base.module.ProtonModule;
+import net.minecraft.util.Identifier;
 
 import static io.github.protonmc.proton.Proton.identifier;
 
@@ -24,4 +26,34 @@ public class DataHandler {
                 m.registerData(dataHandler);
         });
     }
+
+    public void generateSimpleBlockLoot(String base) {
+        generateSimpleBlockLoot(base, false);
+    }
+
+    public void generateSimpleBlockLoot(String base, boolean silkTouch) {
+        pack.addLootTable(identifier("blocks/" + base), lootTableBuilder -> {
+            lootTableBuilder.type(new Identifier("minecraft:block"));
+            lootTableBuilder.pool(pool -> {
+                pool.rolls(1);
+                pool.entry(entry -> entry.type(new Identifier("minecraft:item")).name(identifier(base)));
+                // Absolutely cursed
+                if (!silkTouch) {
+                    pool.condition(new Identifier("minecraft:survives_explosion"), jsonObjectBuilder -> {});
+                } else {
+                    pool.condition(new Identifier("minecraft:match_tool"), jsonObjectBuilder -> {
+                        jsonObjectBuilder.addObject("predicate", predicateObjectBuilder -> {
+                            predicateObjectBuilder.addArray("enchantments", enchantmentArrayBuilder -> {
+                                enchantmentArrayBuilder.addObject(enchantmentObjectBuilder -> {
+                                    enchantmentObjectBuilder.add("enchantment", "minecraft:silk_touch");
+                                    enchantmentObjectBuilder.addObject("levels", levelsObjectBuilder -> levelsObjectBuilder.add("min", 1));
+                                });
+                            });
+                        });
+                    });
+                }
+            });
+        });
+    }
+
 }
