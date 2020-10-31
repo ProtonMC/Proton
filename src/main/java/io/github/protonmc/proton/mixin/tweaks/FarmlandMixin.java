458b346762e0a4634b894f2bb67fa37d4858312b
kara-b <dyon@dyonb.nl>
2020-10-31 11:37:00 +0100

    Don't break farmland when you have feather falling.

---
+++ src/main/java/io/github/protonmc/proton/mixin/tweaks/FarmlandMixin.java
@@ -0,0 +1,38 @@
+package io.github.protonmc.proton.mixin.tweaks;
+
+import io.github.protonmc.proton.base.module.ModuleManager;
+import io.github.protonmc.proton.module.tweaks.FeatherFallingFarmland;
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Mixin(FarmlandBlock.class)
+public class FarmlandMixin {
+
+    @Inject(method = "onLandedUpon", at = @At("HEAD"), cancellable = true)
+    public void onLandedUpon(World world, BlockPos pos, Entity entity, float distance, CallbackInfo ci) {
+        if (ModuleManager.getInstance().isModuleEnabled(FeatherFallingFarmland.class)) {
+            AtomicBoolean hasFeatherFalling = new AtomicBoolean(false);
+            entity.getArmorItems().forEach(itemStack -> {
+                if (!hasFeatherFalling.get()) {
+                    hasFeatherFalling.set(EnchantmentHelper.getLevel(Enchantments.FEATHER_FALLING, itemStack) != 0);
+                }
+            });
+
+            if (hasFeatherFalling.get()) {
+                entity.handleFallDamage(distance, 1.0F);
+                ci.cancel();
+            }
+        }
+    }
+
+}
