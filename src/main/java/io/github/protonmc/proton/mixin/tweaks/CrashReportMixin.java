5fedb1123d01158f482550646d0a1c290d5e5679
YTG1234 <54103478+YTG1234@users.noreply.github.com>
2020-11-04 08:20:52 +0200

    Remove unused code

--- src/main/java/io/github/protonmc/proton/mixin/tweaks/CrashReportMixin.java
+++ src/main/java/io/github/protonmc/proton/mixin/tweaks/CrashReportMixin.java
@@ -7,26 +7,11 @@ import net.minecraft.util.crash.CrashReport;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import static io.github.protonmc.proton.module.tweaks.MoreWittyMessages.wittyMessages;
 
 @Mixin(CrashReport.class)
 public abstract class CrashReportMixin {
-//    @Redirect(method = "generateWittyComment()Ljava/lang/String;", at = @At(value = "NEW", target = "[java/lang/String"))
-//    private static String[] a(String[] orig) {
-//        if (ModuleManager.getInstance().isModuleEnabled(MoreWittyMessages.class)) {
-//            String[] newStrings = new String[orig.length + wittyMessages.size()];
-//            System.arraycopy(orig, 0, newStrings, 0, orig.length);
-//            for (int i = 0; i < wittyMessages.size(); i++) {
-//                newStrings[i + orig.length - 1] = wittyMessages.get(i);
-//            }
-//            return newStrings;
-//        } else {
-//            return orig;
-//        }
-//    }
-
     @ModifyVariable(method = "generateWittyComment()Ljava/lang/String;", at = @At(value = "STORE", ordinal = 0), ordinal = 0)
     @FromModule(MoreWittyMessages.class)
     private static String[] stuff(String[] orig) {
