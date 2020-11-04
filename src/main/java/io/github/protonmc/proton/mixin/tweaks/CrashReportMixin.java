3aabb34371c5b038744bbfe27adc344e612a0fb6
YTG1234 <54103478+YTG1234@users.noreply.github.com>
2020-11-04 22:29:28 +0200

    Rename

--- src/main/java/io/github/protonmc/proton/mixin/tweaks/CrashReportMixin.java
+++ src/main/java/io/github/protonmc/proton/mixin/tweaks/CrashReportMixin.java
@@ -2,27 +2,27 @@ package io.github.protonmc.proton.mixin.tweaks;
 
 import io.github.protonmc.proton.base.annotation.FromModule;
 import io.github.protonmc.proton.base.module.ModuleManager;
-import io.github.protonmc.proton.module.tweaks.MoreWittyMessages;
+import io.github.protonmc.proton.module.tweaks.CustomCrashComments;
 import net.minecraft.util.crash.CrashReport;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import static io.github.protonmc.proton.module.tweaks.MoreWittyMessages.wittyMessages;
+import static io.github.protonmc.proton.module.tweaks.CustomCrashComments.customCrashComments;
 
 @Mixin(CrashReport.class)
 public abstract class CrashReportMixin {
     @ModifyVariable(method = "generateWittyComment()Ljava/lang/String;", at = @At(value = "STORE", ordinal = 0), ordinal = 0)
-    @FromModule(MoreWittyMessages.class)
-    private static String[] addWittyComments(String[] orig) {
-        if (ModuleManager.getInstance().isModuleEnabled(MoreWittyMessages.class)) {
-            if (!MoreWittyMessages.replace) {
-                String[] newStrings = new String[orig.length + wittyMessages.length];
+    @FromModule(CustomCrashComments.class)
+    private static String[] addCustomCrashMessages(String[] orig) {
+        if (ModuleManager.getInstance().isModuleEnabled(CustomCrashComments.class)) {
+            if (!CustomCrashComments.replace) {
+                String[] newStrings = new String[orig.length + customCrashComments.length];
                 System.arraycopy(orig, 0, newStrings, 0, orig.length);
-                System.arraycopy(wittyMessages, 0, newStrings, orig.length - 1, wittyMessages.length);
+                System.arraycopy(customCrashComments, 0, newStrings, orig.length - 1, customCrashComments.length);
                 return newStrings;
             }
-            return wittyMessages;
+            return customCrashComments;
         } else {
             return orig;
         }
--- src/main/java/io/github/protonmc/proton/module/tweaks/CustomCrashComments.java
+++ src/main/java/io/github/protonmc/proton/module/tweaks/CustomCrashComments.java
@@ -4,19 +4,19 @@ import io.github.protonmc.proton.Proton;
 import io.github.protonmc.proton.base.module.ProtonModule;
 import io.github.protonmc.tiny_config.Configurable;
 
-public class MoreWittyMessages extends ProtonModule {
+public class CustomCrashComments extends ProtonModule {
     @Configurable
-    public static String[] wittyMessages = {
+    public static String[] customCrashComments = {
             "OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!"
     };
 
     @Configurable
-    public static boolean replace = false;
+    public static boolean replace = true;
 
     /**
      * Reads config, sets up fields and constructs a ProtonModule.
      */
-    public MoreWittyMessages() {
-        super(Proton.identifier("more_witty_messages"));
+    public CustomCrashComments() {
+        super(Proton.identifier("custom_crash_comments"));
     }
 }
