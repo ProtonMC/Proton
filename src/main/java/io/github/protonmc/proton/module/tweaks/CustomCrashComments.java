5dae68e9d1a9a7263a2b724bfa9b7fa29ddf481b
YTG123 <54103478+YTG1234@users.noreply.github.com>
2020-11-07 10:37:39 +0200

    Made the custom crash comments module disabled by default.

--- src/main/java/io/github/protonmc/proton/module/tweaks/CustomCrashComments.java
+++ src/main/java/io/github/protonmc/proton/module/tweaks/CustomCrashComments.java
@@ -1,9 +1,11 @@
 package io.github.protonmc.proton.module.tweaks;
 
 import io.github.protonmc.proton.Proton;
+import io.github.protonmc.proton.base.annotation.DisabledByDefault;
 import io.github.protonmc.proton.base.module.ProtonModule;
 import io.github.protonmc.tiny_config.Configurable;
 
+@DisabledByDefault
 public class CustomCrashComments extends ProtonModule {
     @Configurable
     public static String[] customCrashComments = {
